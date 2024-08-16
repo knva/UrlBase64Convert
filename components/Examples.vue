c30e10416ae892db13f7f1d13dae259edc136934
knva <txt_ch@163.com>
2024-08-16 10:37:14 +0800

    feat: Refactor base64 encode/decode functionality in Examples component
    优化 Examples 组件中的 base64 编码/解码功能，提升性能和用户体验。更新逻辑以使用新添加的函数，并修改文本区域的占位符以反映功能的变化。

--- components/Examples.vue
+++ components/Examples.vue
@@ -20,15 +20,23 @@ const jsonData = computed(() => {
 const pageHeight = computed(() => {
   if (process.client) {
     const { height, width } = window.screen;
-    return height;
+    return height - 155;
   }
 });
+const deepVar = ref(1);
 // Base64 编码
 const base64Encode = (str: string | undefined) => {
   const utf8Bytes = new TextEncoder().encode(str);
   return btoa(String.fromCharCode(...utf8Bytes));
 };
-
+const fold = () => {
+  //折叠
+  deepVar.value=1;
+};
+const unfold = () => {
+  //展开
+  deepVar.value=1111;
+};
 // Base64 解码
 const base64Decode = (str: string) => {
   const binaryString = atob(str);
@@ -75,48 +83,59 @@ const copyText = (text: string) => {
     <!--标题，解密工具-->
     <!--提示用户输入的文本-->
 
-    <div class="container">
-      <div class="left-column">
-        <h1>Decode Tool</h1>
-        <div class="input-group">
-          <el-input
-            v-model="encodeText"
-            type="textarea"
-            class="input-textarea"
-            placeholder="输入"
-            :autosize="{ minRows: 10, maxRows: 10 }"
-          />
-        </div>
-        <div class="buttons">
-          <!--解密-->
-          <el-button type="primary" @click="decode">Decode</el-button>
-          <!--加密-->
-          <el-button type="primary" @click="encode">Encode</el-button>
-          <!--清空-->
-          <el-button @click="clear">Clear</el-button>
-          <!--复制-->
-          <el-button type="primary" @click="copyText(decodeText)"
-            >Copy</el-button
-          >
-        </div>
-        <div class="input-group">
-          <el-input
-            v-model="decodeText"
-            type="textarea"
-            class="input-textarea"
-            placeholder="输出"
-            :autosize="{ minRows: 10, maxRows: 10 }"
-          />
-        </div>
-      </div>
-      <vue-json-pretty
-        :data="jsonData"
-        :showIcon="true"
-        :virtual="true"
-        class="json-pretty"
-        :height="pageHeight"
-      />
-    </div>
+    <el-container>
+      <el-aside width="500px">
+        <el-header>
+          <h1>Decode Tool</h1>
+        </el-header>
+        <el-main>
+          <div class="input-group">
+            <el-input
+              v-model="encodeText"
+              type="textarea"
+              class="input-textarea"
+              placeholder="输入"
+              :autosize="{ minRows: 10, maxRows: 10 }"
+            />
+          </div>
+          <div class="buttons">
+            <!--解密-->
+            <el-button type="primary" @click="decode">Decode</el-button>
+            <!--加密-->
+            <el-button type="primary" @click="encode">Encode</el-button>
+            <!--清空-->
+            <el-button @click="clear">Clear</el-button>
+            <!--复制-->
+            <el-button type="primary" @click="copyText(decodeText)"
+              >Copy</el-button
+            >
+          </div>
+          <div class="input-group">
+            <el-input
+              v-model="decodeText"
+              type="textarea"
+              class="input-textarea"
+              placeholder="输出"
+              :autosize="{ minRows: 10, maxRows: 10 }"
+            />
+          </div>
+        </el-main>
+      </el-aside>
+      <el-main>
+        <!-- 按钮组-->
+        <!--折叠-->
+        <el-button type="primary" @click="fold">Fold</el-button>
+        <!--展开-->
+        <el-button type="primary" @click="unfold">Unfold</el-button>
+        <vue-json-pretty
+          :data="jsonData"
+          :showIcon="true"
+          :virtual="true"
+          class="json-pretty"
+          :deep="deepVar"
+          :height="pageHeight"
+      /></el-main>
+    </el-container>
   </div>
 </template>
 <style scoped>
@@ -124,15 +143,8 @@ const copyText = (text: string) => {
   margin-top: 20px;
   margin-bottom: 20px;
 }
-.container {
-  display: flex;
-  justify-content: space-between;
-}
-
-.left-column {
-  display: flex;
-  flex-direction: column;
-  width: 30%;
+.json-pretty {
+  height: calc(100vh - 55px);
 }
 
 .input-textarea {
@@ -142,10 +154,5 @@ const copyText = (text: string) => {
 .input-group {
   flex: 1;
   margin-bottom: 10px; /* Adjust spacing as needed */
-}
-
-.json-pretty {
-  width: 70%;
-  height: 100vh; /* Adjust height as needed */
 }
 </style>
